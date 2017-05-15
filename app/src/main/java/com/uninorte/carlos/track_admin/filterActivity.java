bc254d14461295e2efaec6203bd3495c29d9cc96
Antonio <antony9409@gmail.com>
2017-05-15 02:01:34 -0500

    Spinners

--- app/src/main/java/com/uninorte/carlos/track_admin/filterActivity.java
+++ app/src/main/java/com/uninorte/carlos/track_admin/filterActivity.java
@@ -3,18 +3,33 @@ package com.uninorte.carlos.track_admin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class filterActivity extends AppCompatActivity {
     final Calendar c = Calendar.getInstance();
-
+    public Spinner diai,mesi,añoi,diaf,mesf,añof,horai,mini,horaf,minf;
+    int diain,mesin,añoin,diafi,mesfi,añofi;
+    String horain,minin,horafi,minfi,key,key2;
     int maxYear = c.get(Calendar.YEAR) - 20; // this year ( 2011 ) - 20 = 1991
     int maxMonth = c.get(Calendar.MONTH);
     int maxDay = c.get(Calendar.DAY_OF_MONTH);
-
+    private DatabaseReference mDatabase;
+    private String TAG= "FirebaseLog";
     int minYear = 1960;
     int minMonth = 0; // january
     int minDay = 25;
@@ -23,10 +38,208 @@ public class filterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        key=getIntent().getStringExtra("name");
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        diai = (Spinner) findViewById(R.id.diai);
+        diaf = (Spinner) findViewById(R.id.diaf);
+        mesi = (Spinner) findViewById(R.id.mesi);
+        mesf = (Spinner) findViewById(R.id.mesf);
+        añoi = (Spinner) findViewById(R.id.añoi);
+        añof = (Spinner) findViewById(R.id.añof);
+        horai = (Spinner) findViewById(R.id.horai);
+        mini = (Spinner) findViewById(R.id.mini);
+        horaf = (Spinner) findViewById(R.id.horaf);
+        minf = (Spinner) findViewById(R.id.minf);
+        List<String> dias = new ArrayList<String>();
+        for(int i=0;i<31;i++){
+            dias.add(""+(i+1));
+        }
+        ArrayAdapter<String> dia1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dias);
+        dia1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        diai.setAdapter(dia1);
+        diai.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                diain = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                diain=0;
+            }
+        });
+        ArrayAdapter<String> dia2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dias);
+        dia2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        diaf.setAdapter(dia2);
+        diaf.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                diafi = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                diafi=0;
+            }
+        });
+        List<String> meses = new ArrayList<String>();
+        for(int i=0;i<12;i++){
+            meses.add(""+(i+1));
+        }
+        ArrayAdapter<String> mes1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, meses);
+        mes1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mesi.setAdapter(mes1);
+        mesi.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mesin = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mesin=0;
+            }
+        });
+        ArrayAdapter<String> mes2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, meses);
+        mes2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mesf.setAdapter(mes2);
+        mesf.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mesfi = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mesfi=0;
+            }
+        });
+        List<String> años = new ArrayList<String>();
+        for(int i=2010;i<2017;i++){
+            años.add(""+(i+1));
+        }
+        ArrayAdapter<String> año1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, años);
+        año1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        añoi.setAdapter(año1);
+        añoi.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                añoin = Integer.parseInt(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                añoin=0;
+            }
+        });
+        ArrayAdapter<String> año2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, años);
+        año2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        añof.setAdapter(año2);
+        añof.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                añofi = Integer.parseInt(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                añofi=0;
+            }
+        });
+        List<String> horas = new ArrayList<String>();
+        for(int i=0;i<=23;i++){
+            if(i<10) {
+                horas.add("0" + (i));
+            }else{
+                horas.add("" + (i));
+            }
+        }
+        ArrayAdapter<String> hora1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, horas);
+        hora1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        horai.setAdapter(hora1);
+        horai.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                horain = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                horain="";
+            }
+        });
+        ArrayAdapter<String> hora2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, horas);
+        hora2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        horaf.setAdapter(hora2);
+        horaf.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                horafi = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                horafi="";
+            }
+        });
+        List<String> mins = new ArrayList<String>();
+        for(int i=0;i<=59;i++){
+            if(i<10) {
+                mins.add("0" + (i));
+            }else{
+                mins.add("" + (i));
+            }
+        }
+        ArrayAdapter<String> min1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mins);
+        min1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mini.setAdapter(min1);
+        mini.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                minin = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                minin="";
+            }
+        });
+        ArrayAdapter<String> min2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mins);
+        min2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minf.setAdapter(min2);
+        minf.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                minfi = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                minfi="";
+            }
+        });
     }
 
     public void onClick(View view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data:dataSnapshot.getChildren()
+                        ) {
+                    if(data.getKey().equals(key)) {
+                        Log.d(TAG, "onDataChange: " + data.getValue());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         Intent intent = new Intent(this,MapsActivity.class);
         intent.putExtra("name",getIntent().getStringExtra("name"));
         startActivity(intent);
